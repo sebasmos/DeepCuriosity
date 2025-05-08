c5f36f2d2ce4a8165fbc78b014db0f22fd677096
yjmantilla <yjmantilla@gmail.com>
2025-05-08 16:07:37 -0400

    Update SLURM job script to use Python 3.11.5 and add requirements_cc.txt

--- ccjob.sh
+++ ccjob.sh
@@ -9,13 +9,14 @@
 #SBATCH --mail-user=yjmantilla@gmail.com
 #SBATCH --mail-type=ALL
 
-module load python/3.11.11
-virtualenv --no-download $SLURM_TMPDIR/env
+module load python/3.11.5
+#virtualenv --no-download $SLURM_TMPDIR/env
+virtualenv $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
 pip install --no-index --upgrade pip
 cd /home/yorguin/scratch/DeepCuriosity
 ##pip install --no-index -r requirements.txt
-pip install -r requirements.txt
+pip install -r requirements_cc.txt
 python baseline.py --config configs/config.yaml --variant default
 python icm.py --config configs/config.yaml --variant icm
